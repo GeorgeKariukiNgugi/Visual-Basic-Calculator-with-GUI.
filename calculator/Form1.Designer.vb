d8d829a8787cac55598ac8cb84eb1f784077dbeb
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2018-09-26 16:49:42 +0300

    Initial commit

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Enabled = False
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 28.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(40, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(272, 55)
+        Me.Button1.TabIndex = 0
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "0."
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(25, 105)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(58, 36)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "7"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(107, 105)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(58, 36)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "8"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button4.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(194, 105)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(58, 36)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "9"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button5.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(271, 105)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(58, 36)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "/"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button6.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(25, 170)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(58, 36)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "4"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button7.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(107, 170)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(58, 36)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "5"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button8.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(194, 170)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(58, 36)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "6"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button9.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(271, 170)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(58, 36)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "*"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button10.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(25, 236)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(58, 36)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "1"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button11.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button11.Location = New System.Drawing.Point(116, 236)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(58, 36)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "2"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button12.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button12.Location = New System.Drawing.Point(194, 236)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(58, 36)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "3"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button13.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button13.Location = New System.Drawing.Point(271, 236)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(58, 36)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "-"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button14.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button14.Location = New System.Drawing.Point(25, 304)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(58, 36)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "0"
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button15.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button15.Location = New System.Drawing.Point(116, 304)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(58, 36)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "."
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button16.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button16.Location = New System.Drawing.Point(194, 304)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(58, 36)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "+"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.MediumSpringGreen
+        Me.Button17.Font = New System.Drawing.Font("Modern No. 20", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button17.Location = New System.Drawing.Point(271, 304)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(75, 118)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "="
+        Me.Button17.UseVisualStyleBackColor = False
+        '
+        'Button18
+        '
+        Me.Button18.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button18.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button18.Location = New System.Drawing.Point(25, 362)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(94, 64)
+        Me.Button18.TabIndex = 17
+        Me.Button18.Text = "Clear"
+        Me.Button18.UseVisualStyleBackColor = False
+        '
+        'Button19
+        '
+        Me.Button19.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button19.Font = New System.Drawing.Font("Modern No. 20", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button19.Location = New System.Drawing.Point(147, 362)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(94, 64)
+        Me.Button19.TabIndex = 18
+        Me.Button19.Text = "Exit"
+        Me.Button19.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Cyan
+        Me.ClientSize = New System.Drawing.Size(358, 451)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents Button19 As System.Windows.Forms.Button
+
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,223 @@
+﻿Public Class Form1
+    Dim value1 As String = ""
+    Dim value2 As String = ""
+    Dim value3 As Double = 0.0
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button18_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button18.Click
+        value1 = ""
+        value2 = ""
+        value3 = 0.0
+        Button1.Text = 0.0
+
+
+    End Sub
+
+    Private Sub Button19_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button19.Click
+        End
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        If value1 > "" And value2 = "+" Then
+            Button1.Text = Val(value1) + Val(Button1.Text)
+            value3 = Button1.Text
+        ElseIf value1 > "" And value2 = "-" Then
+            Button1.Text = Val(value1) - Val(Button1.Text)
+            value3 = Button1.Text
+        ElseIf value1 > "" And value2 = "*" Then
+            Button1.Text = Val(value1) * Val(Button1.Text)
+            value3 = Button1.Text
+        ElseIf value1 > "" And value2 = "/" Then
+            Button1.Text = Val(value1) / Val(Button1.Text)
+            value3 = Button1.Text
+        Else
+
+
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        value2 = sender.text
+        value1 = Button1.Text
+
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        value2 = sender.text
+        value1 = Button1.Text
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        value2 = sender.text
+        value1 = Button1.Text
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        value2 = sender.text
+        value1 = Button1.Text
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        If Button1.Text = value1 Then
+            Button1.Text = "0."
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = "0."
+        ElseIf Button1.Text = "0" Then
+            Button1.Text = "0."
+        ElseIf Button1.Text = value1 Then
+            Button1.Text = "0."
+        Else : Button1.Text = Button1.Text & "0"
+
+        End If
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        If Button1.Text = "0." Then
+            Button1.Text = "."
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = "."
+        ElseIf Button1.Text = value1 Then
+            Button1.Text = "."
+        Else
+            If Button1.Text.Contains(".") Then
+            Else
+                Button1.Text = Button1.Text & "."
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If Button1.Text = value1 Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = "0." Then
+            Button1.Text = sender.text
+        ElseIf Button1.Text = value3 Then
+            Button1.Text = sender.text
+        Else
+            Button1.Text = Button1.Text & sender.text
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button18_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button18.DoubleClick
+        Application.Exit()
+
+
+    End Sub
+
+    Private Sub Form1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.DoubleClick
+        End
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
